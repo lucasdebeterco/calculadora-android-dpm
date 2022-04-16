2ff36d0edb8cbd73d3512fa6bb07b37480705f8e
Lucas Debeterco <lucasdebeterco@gmail.com>
2022-04-16 12:07:01 -0300

    Pegando e apresentando valores dos botões

--- app/src/main/java/com/example/calculadora/MainActivity.java
+++ app/src/main/java/com/example/calculadora/MainActivity.java
@@ -20,8 +20,41 @@ public class MainActivity extends AppCompatActivity {
         String number = ed1.getText().toString();
 
         switch (view.getId()) {
+            case R.id.btn1:
+                number += '1';
+                break;
+            case R.id.btn2:
+                number += '2';
+                break;
+            case R.id.btn3:
+                number += '3';
+                break;
+            case R.id.btn4:
+                number += '4';
+                break;
+            case R.id.btn5:
+                number += '5';
+                break;
+            case R.id.btn6:
+                number += '6';
+                break;
             case R.id.btn7:
                 number += '7';
+                break;
+            case R.id.btn8:
+                number += '8';
+                break;
+            case R.id.btn9:
+                number += '9';
+                break;
+            case R.id.btn0:
+                number += '0';
+                break;
+            case R.id.btnPonto:
+                number += '.';
+                break;
+            case R.id.btnPlusMinus:
+                number = '-' + number;
                 break;
         }
 
